419113591786b50666f28a2605dd410a6df09db4
Edgar Dominguez <edgar.dominguez5434@alumnos.udg.mx>
2020-11-27 00:01:03 -0600

    Visualizar la informacion en una App de Ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PeliculasService } from '../api/peliculas.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  peliculas = []; /*[
+    {
+      titulo: "Bob Esponja al rescate",
+      anio: 2020,
+      director:"Tim Hill",
+      imagen:"https://as01.epimg.net/meristation/imagenes/2019/10/25/noticias/1572002941_916672_1572003064_noticia_normal.jpg"
+    },
+    {
+      titulo: "Batman: Death in the Family",
+      anio: 2020,
+      director:"Brandon Vietti",
+      imagen:"https://www.dccomics.com/sites/default/files/DeathFamily_marquee_5f8101ee87bee8.17833087.jpg"
+    }
+  ];*/
 
-  constructor() {}
+  constructor(private peliculasService: PeliculasService) {}
 
+  ngOnInit(){
+    this.peliculasService.peliculas.subscribe(peliculas => {
+      this.peliculas = peliculas;
+    });
+    //this.peliculasService.getPeliculas();
+  }
 }
